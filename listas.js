d57c20bb28aa7a66b1b4f6932f53990def325014
danielalvaro <32547125+danielalvaro@users.noreply.github.com>
2018-04-10 21:00:50 +0200

    primer commit

---
+++ listas.js
@@ -0,0 +1,6 @@
+//Pila: Solo puede sacarse el último elemento.
+//Cola: Permite insertar, extraer o comprobar si está vacía.
+//Lista: Se permite insertar o extraer cualquier elemento.
+
+
+function
